fff5f11ea5b04dc0ca1388c5a7ea280fd4506788
miraclegalaxys <galaxypop.sa01@gmail.com>
2023-11-26 01:06:49 +0700

    Update code

--- Client/Client.ino
+++ Client/Client.ino
@@ -1,13 +1,18 @@
 #include <WiFi.h>
 #include <WebServer.h>
-
-const char* ssid = "ggalaxy_2.4G";
-const char* password = "021146pop";
-const char* hostESP32_1 = "192.168.1.111"; // IP Address ของ ESP-32(1)
+#include <U8g2lib.h>
+//---------------------------------------------------------------------------------------------------------------
+const char* ssid = "Test";
+const char* password = "123456789";
+const char* hostESP32_1 = "192.168.137.74"; // IP Address ของ ESP-32(1)
 const int ledPin = 25; // Change as per your setup
-
+//---------------------------------------------------------------------------------------------------------------
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+//---------------------------------------------------------------------------------------------------------------
+String host = String(hostESP32_1);
+//---------------------------------------------------------------------------------------------------------------
 WebServer server(80);
-
+//---------------------------------------------------------------------------------------------------------------
 String getHtmlPage() {
   String strHtml = "<html><head>";
   strHtml += "<style>";
@@ -22,9 +27,22 @@ String getHtmlPage() {
   strHtml += "</body></html>";
   return strHtml;
 }
+//---------------------------------------------------------------------------------------------------------------
+void displayMessage() {
+  String message = server.arg("message"); // รับข้อความที่ส่งมา
 
+  u8g2.clearBuffer();         
+  u8g2.setFont(u8g2_font_ncenB08_tr);
+  u8g2.drawStr(0, 10, message.c_str()); // แสดงข้อความบน OLED
+  u8g2.sendBuffer();
+
+  server.send(200, "text/plain", "Message displayed: " + message);
+}
+
+//---------------------------------------------------------------------------------------------------------------
 void setup() {
   Serial.begin(115200);
+  u8g2.begin();
   pinMode(ledPin, OUTPUT);
 
   WiFi.begin(ssid, password);
@@ -34,43 +52,52 @@ void setup() {
   }
   Serial.println("Connected to WiFi ");
   Serial.println(WiFi.localIP());
-
+//---------------------------------------------------------------------------------------------------------------
 
  server.on("/", []() {
     server.send(200, "text/html", getHtmlPage());
   });
-
+//---------------------------------------------------------------------------------------------------------------
   server.on("/on", []() {
     // ส่งคำขอไปยัง ESP-32(1) เพื่อเปิด LED
     WiFiClient client;
-    if (client.connect("192.168.1.111", 80)) {
-      client.print("GET /turnOnLED HTTP/1.1\r\nHost: 192.168.1.111\r\nConnection: close\r\n\r\n");
+    if (client.connect(hostESP32_1, 80)) {
+      client.print("GET /turnOnLED HTTP/1.1\r\nHost: " + host + "\r\nConnection: close\r\n\r\n");
     }
     server.send(200, "text/plain", "Sent ON command to ESP-32(1)");
   });
-
-  server.on("/off", []() {
+//---------------------------------------------------------------------------------------------------------------
+  /*server.on("/off", []() {
     // ส่งคำขอไปยัง ESP-32(1) เพื่อปิด LED
     WiFiClient client;
-    if (client.connect("192.168.1.111", 80)) {
-      client.print("GET /turnOffLED HTTP/1.1\r\nHost: 192.168.1.111\r\nConnection: close\r\n\r\n");
+    if (client.connect(hostESP32_1, 80)) {
+      client.print("GET /turnOffLED HTTP/1.1\r\nHost: " + host + "\r\nConnection: close\r\n\r\n");
     }
     server.send(200, "text/plain", "Sent OFF command to ESP-32(1)");
-  });
-
+  });*/
+//---------------------------------------------------------------------------------------------------------------
   server.on("/turnOnLED", []() {
     digitalWrite(ledPin, HIGH);
     server.send(200, "text/plain", "LED is ON");
   });
-
-  server.on("/turnOffLED", []() {
+//---------------------------------------------------------------------------------------------------------------
+  /*server.on("/turnOffLED", []() {
     digitalWrite(ledPin, LOW);
     server.send(200, "text/plain", "LED is OFF");
-  });
+  });*/
+//---------------------------------------------------------------------------------------------------------------
+
+server.on("/display", displayMessage); // ตั้งค่าเส้นทางสำหรับการแสดงข้อความ
+ //---------------------------------------------------------------------------------------------------------------
 
   server.begin();
+//---------------------------------------------------------------------------------------------------------------
 }
-
 void loop() {
   server.handleClient();
+
+  //----------------------------------------------------------------------------------------------------
+
+
+  //---------------------------------------------------------------------------------------------------------------
 }
--- Station/Station.ino
+++ Station/Station.ino
@@ -1,23 +1,22 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WebServer.h>
-
+//---------------------------------------------------------------------------------------------------------------
 // WiFi credentials
-const char* ssid = "ggalaxy_2.4G";
-const char* password = "021146pop";
-const char* host = "192.168.1.112";
+const char* ssid = "Test";
+const char* password = "123456789";
+const char* host = "192.168.137.32";
 const int ledPin = 25;
-
 const int buttonPin = 27; // Change as per your setup
 bool ledState = false;
-
+//---------------------------------------------------------------------------------------------------------------
 WebServer server(80);
-
+//---------------------------------------------------------------------------------------------------------------
 void setup() {
   Serial.begin(115200);
   pinMode(buttonPin, INPUT);
   pinMode(ledPin, OUTPUT);
-
+//---------------------------------------------------------------------------------------------------------------
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -26,46 +25,56 @@ void setup() {
   }
   Serial.println("Connected to WiFi");
   Serial.println(WiFi.localIP());
-
+//---------------------------------------------------------------------------------------------------------------
 
 
 server.on("/turnOnLED", []() {
     digitalWrite(ledPin, HIGH);
     server.send(200, "text/plain", "LED is ON");
   });
-
+//---------------------------------------------------------------------------------------------------------------
   server.on("/turnOffLED", []() {
     digitalWrite(ledPin, LOW);
     server.send(200, "text/plain", "LED is OFF");
   });
-
+//---------------------------------------------------------------------------------------------------------------
   server.begin();
-
+//---------------------------------------------------------------------------------------------------------------
 }
 
 void loop() {
   int buttonState = digitalRead(buttonPin);
-
+//---------------------------------------------------------------------------------------------------------------
   // ตรวจสอบว่าปุ่มถูกกด
   if (buttonState == HIGH) {
     ledState = !ledState; // สลับสถานะ LED
-
+//---------------------------------------------------------------------------------------------------------------
     WiFiClient client;
     if (!client.connect(host, 80)) {
       Serial.println("Connection to ESP-32(2) failed");
       return;
     }
-
+//---------------------------------------------------------------------------------------------------------------
     // ส่งคำขอตามสถานะ LED
     if (ledState) {
       client.print(String("GET /turnOnLED HTTP/1.1\r\n") +
                    "Host: " + host + "\r\n" +
                    "Connection: close\r\n\r\n");
-    } else {
+    } /*else {
       client.print(String("GET /turnOffLED HTTP/1.1\r\n") +
                    "Host: " + host + "\r\n" +
                    "Connection: close\r\n\r\n");
+    }*/
+//---------------------------------------------------------------------------------------------------------------
+    if (client.connect(host, 80)) {
+    String message = "Hello OLED"; // ข้อความที่ต้องการส่ง
+    client.print(String("GET /display?message=") + message + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: close\r\n\r\n");
     }
-  }
+  //---------------------------------------------------------------------------------------------------------------
   server.handleClient();
+  //---------------------------------------------------------------------------------------------------------------
+}
+
 }
